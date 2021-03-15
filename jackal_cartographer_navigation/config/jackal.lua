e1acfd9fb370a884e0f1c9ebac2801a2ff8385cb
User1 Nuc40 <user1@nuc40.com>
2021-03-15 16:45:49 -0400

    b4 integrating fix"

--- jackal_cartographer_navigation/config/jackal.lua
+++ jackal_cartographer_navigation/config/jackal.lua
@@ -43,12 +43,17 @@ options = {
 }
 
 MAP_BUILDER.use_trajectory_builder_2d = true
+TRAJECTORY_BUILDER_2D.min_range = 0.3 -- minimal distance Point Cloud -> 0.25 ouster specs
+TRAJECTORY_BUILDER_2D.max_range = 50. -- maximal distance Point Cloud ->120 ouster specs
+
+
+
 
 TRAJECTORY_BUILDER_2D.num_accumulated_range_data = 1
 
 TRAJECTORY_BUILDER_2D.min_range = 0.3
 TRAJECTORY_BUILDER_2D.missing_data_ray_length = 2.
-TRAJECTORY_BUILDER_2D.use_imu_data = false
+TRAJECTORY_BUILDER_2D.use_imu_data = true
 -- TRAJECTORY_BUILDER_2D.use_online_correlative_scan_matching = true
 --TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.linear_search_window = 0.1
 --TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.translation_delta_cost_weight = 10.
