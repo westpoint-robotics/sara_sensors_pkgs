f4fa0e9e4d7983f5accf83cc0c76b0d58bc7e883
User1 Nuc01 <user1@nuc01.com>
2021-03-15 16:47:52 -0400

    working with bagfile

--- jackal_cartographer_navigation/config/jackal.lua
+++ jackal_cartographer_navigation/config/jackal.lua
@@ -19,19 +19,19 @@ options = {
   map_builder = MAP_BUILDER,
   trajectory_builder = TRAJECTORY_BUILDER,
   map_frame = "map",
-  tracking_frame = "base_link", -- original: base_link
-  published_frame = "odom",
+  tracking_frame = "os_imu", -- original: base_link
+  published_frame = "base_link",
   odom_frame = "odom",
-  provide_odom_frame = false, -- original: false
-  publish_frame_projected_to_2d = false,
-  use_odometry = true, -- original: true
+  provide_odom_frame = true, -- original: false
+  publish_frame_projected_to_2d = true,
+  use_odometry = false, -- original: true
   use_nav_sat = false,
   use_landmarks = false,
-  num_laser_scans = 1,
+  num_laser_scans = 0,
   num_multi_echo_laser_scans = 0,
   num_subdivisions_per_laser_scan = 1,
-  num_point_clouds = 0,
-  lookup_transform_timeout_sec = 1.,
+  num_point_clouds = 1,
+  lookup_transform_timeout_sec = 0.2,
   submap_publish_period_sec = 0.3,
   pose_publish_period_sec = 5e-3,
   trajectory_publish_period_sec = 30e-3,
@@ -46,43 +46,51 @@ MAP_BUILDER.use_trajectory_builder_2d = true
 
 TRAJECTORY_BUILDER_2D.num_accumulated_range_data = 1
 
-TRAJECTORY_BUILDER_2D.min_range = 0.3
-TRAJECTORY_BUILDER_2D.missing_data_ray_length = 2.
-TRAJECTORY_BUILDER_2D.use_imu_data = false -- original: false
+TRAJECTORY_BUILDER_2D.min_range = 0.5
+TRAJECTORY_BUILDER_2D.max_range = 50.
+
+
+--TRAJECTORY_BUILDER_2D.missing_data_ray_length = 2.
+--TRAJECTORY_BUILDER_2D.use_imu_data = false -- original: false
 -- TRAJECTORY_BUILDER_2D.use_online_correlative_scan_matching = true
 --TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.linear_search_window = 0.1
 --TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.translation_delta_cost_weight = 10.
 --TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.rotation_delta_cost_weight = 10.
-TRAJECTORY_BUILDER_2D.ceres_scan_matcher.translation_weight = 10
-TRAJECTORY_BUILDER_2D.ceres_scan_matcher.rotation_weight = 15
+--TRAJECTORY_BUILDER_2D.ceres_scan_matcher.translation_weight = 10
+--TRAJECTORY_BUILDER_2D.ceres_scan_matcher.rotation_weight = 15
 
-POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.angular_search_window = math.rad(15.)
-POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.linear_search_window = 3.
+--POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.angular_search_window = math.rad(15.)
+--POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.linear_search_window = 3.
 
-POSE_GRAPH.optimization_problem.huber_scale = 1e2
+--POSE_GRAPH.optimization_problem.huber_scale = 1e2
 
 -----------------TUNE THESE PARAMETERS FOR LOW LATENCY-------------------------------
-
+POSE_GRAPH.optimize_every_n_nodes = 0
+POSE_GRAPH.constraint_builder.sampling_ratio = 0.03
+POSE_GRAPH.optimization_problem.ceres_solver_options.max_num_iterations = 100
+POSE_GRAPH.constraint_builder.min_score = 0.8
+POSE_GRAPH.constraint_builder.global_localization_min_score = 0.8
+POSE_GRAPH.optimization_problem.huber_scale = 5e2
 ------------Global SLAM------------
-POSE_GRAPH.optimize_every_n_nodes = 100 -- Decrease
-MAP_BUILDER.num_background_threads = 4 -- Increase up to number of cores
-POSE_GRAPH.global_sampling_ratio = 0.003 -- Decrease
-POSE_GRAPH.constraint_builder.sampling_ratio = 0.4 -- Decrease
-POSE_GRAPH.constraint_builder.min_score = 0.85 -- Increase
-POSE_GRAPH.global_constraint_search_after_n_seconds = 30 -- Increase
-TRAJECTORY_BUILDER_2D.ceres_scan_matcher.ceres_solver_options.max_num_iterations = 5 -- Decrease
+--POSE_GRAPH.optimize_every_n_nodes = 0  Decrease 
+--MAP_BUILDER.num_background_threads = 4  Increase up to number of cores
+--POSE_GRAPH.global_sampling_ratio = 0.003 -- Decrease
+--POSE_GRAPH.constraint_builder.sampling_ratio = 0.03  Decrease
+--POSE_GRAPH.constraint_builder.min_score = 0.8  Increase
+--POSE_GRAPH.global_constraint_search_after_n_seconds = 30  Increase
+--TRAJECTORY_BUILDER_2D.ceres_scan_matcher.ceres_solver_options.max_num_iterations = 5 -- Decrease
 
 ---------Global/Local SLAM---------
-TRAJECTORY_BUILDER_2D.adaptive_voxel_filter.min_num_points = 100 -- Decrease
-TRAJECTORY_BUILDER_2D.adaptive_voxel_filter.max_range = 10. -- Decrease
-TRAJECTORY_BUILDER_2D.adaptive_voxel_filter.max_length = 1.0 -- Increase
-TRAJECTORY_BUILDER_2D.loop_closure_adaptive_voxel_filter.min_num_points = 50 -- Decrease
-TRAJECTORY_BUILDER_2D.loop_closure_adaptive_voxel_filter.max_range = 10. -- Decrease
-TRAJECTORY_BUILDER_2D.loop_closure_adaptive_voxel_filter.max_length = 1.8 -- Increase
-TRAJECTORY_BUILDER_2D.voxel_filter_size = 0.05 -- Increase
+--TRAJECTORY_BUILDER_2D.adaptive_voxel_filter.min_num_points = 100 -- Decrease
+--TRAJECTORY_BUILDER_2D.adaptive_voxel_filter.max_range = 10. -- Decrease
+--TRAJECTORY_BUILDER_2D.adaptive_voxel_filter.max_length = 1.0 -- Increase
+--TRAJECTORY_BUILDER_2D.loop_closure_adaptive_voxel_filter.min_num_points = 50 -- Decrease
+--TRAJECTORY_BUILDER_2D.loop_closure_adaptive_voxel_filter.max_range = 10. -- Decrease
+--TRAJECTORY_BUILDER_2D.loop_closure_adaptive_voxel_filter.max_length = 1.8 -- Increase
+--TRAJECTORY_BUILDER_2D.voxel_filter_size = 0.05 -- Increase
 -- TRAJECTORY_BUILDER_2D.submaps.resolution=0.05 -- Increase
-TRAJECTORY_BUILDER_2D.submaps.num_range_data = 100 -- Decrease
-TRAJECTORY_BUILDER_2D.max_range = 10. -- Decrease
+--TRAJECTORY_BUILDER_2D.submaps.num_range_data = 100 -- Decrease
+--TRAJECTORY_BUILDER_2D.max_range = 10. -- Decrease
 
 -------------------------------------------------------------------------------------
 
